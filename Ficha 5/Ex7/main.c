8aa41737886e592b76ec55b49926e954a4f42953
Miguel Costa <nerdxontheline@outlook.com>
2017-11-10 17:54:13 +0000

    Ex7

--- Ficha 5/Ex7/main.c
+++ Ficha 5/Ex7/main.c
@@ -18,21 +18,37 @@
 /*
  * 
  */
+void arrayA(int A[]){
+    int i;
+    for(i=0; i< TAMANHO; ++i){
+        printf("Introduza os valores do Array A: ");
+        scanf("%d", &A[i]);
+    }
+}
+void arrayB(int B[]){
+    int i;
+    for(i=0; i< TAMANHO; ++i){
+        printf("Introduza os valores do Array B: ");
+        scanf("%d", &B[i]);
+    }
+}
+void arrayC(int A[], int B[], int C[]){
+    int i;
+    for(i=0; i< TAMANHO; ++i){
+        C[i]=A[i]+B[i];
+    }
+}
+
 int main(int argc, char** argv) {
     int A[TAMANHO];
     int B[TAMANHO];
-    int i;
+    int C[TAMANHO];
     
-    for(i=0; i< TAMANHO; ++i){
-        printf("Introduza os  valores do Array A: ");
-        scanf("%d", &A[i]);
-    }
-    
-    for(i=0; i< TAMANHO; ++i){
-        printf("Introduza os  valores do Array B: ");
-        scanf("%d", &B[i]);
-    }
-    
+    arrayA(A);
+    puts("");
+    arrayB(B);
+    puts("");
+    arrayC(A,B,C);
     
     return (EXIT_SUCCESS);
 }
