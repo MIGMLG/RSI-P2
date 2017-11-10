fa5a8387f28b69cb678e395fb4d99a92586e1c56
DevMig <nerdxontheline@outlook.com>
2017-11-10 16:46:31 +0000

    Correção

--- Ficha 4/Ex9/main.c
+++ Ficha 4/Ex9/main.c
@@ -13,24 +13,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define valorinicial 0
 
 /*
  * 
  */
+
 int main(int argc, char** argv) {
     int valor;
-    const  int valorinical=0; 
-    
-    
+
     printf("Introduza um valor: ");
     scanf("%d", &valor);
     const  int valorfinal=valor;
-    
-    for (valorinical; (valor-valorfinal)<=valorfinal; ++valor){
+
+    for (valorinicial; (valor-valorfinal)<=valorfinal; ++valor){
         printf("%d", valor-valorfinal);
         puts("");
-    }
-    
+    }  
+
     return (EXIT_SUCCESS);
 }
 
