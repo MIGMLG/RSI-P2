4d42591be1bdcbeb48055e2b6ff761e9c2fdb493
Miguel Costa <nerdxontheline@outlook.com>
2017-11-10 19:26:15 +0000

    Ex7

--- Ficha 5/Ex7/main.c
+++ Ficha 5/Ex7/main.c
@@ -32,6 +32,7 @@ void arrayB(int B[]){
         scanf("%d", &B[i]);
     }
 }
+
 void arrayC(int A[], int B[], int C[]){
     int i;
     for(i=0; i< TAMANHO; ++i){
@@ -39,16 +40,64 @@ void arrayC(int A[], int B[], int C[]){
     }
 }
 
+void arrayD(int A[], int B[], int D[]){
+    int i,j, contador=0, contadorarray=0;
+    for(i=0; i< TAMANHO; ++i){
+        for(j=0; j< TAMANHO; ++j){
+            if(A[i]==B[j]){
+                break;
+            }
+            else{
+                contador+=1;
+            }
+        }
+        if(contador==10){
+            D[contadorarray]=A[i];
+            contadorarray+=1;
+        }
+        contador=0;
+    }
+}
+void arrayE(int A[], int B[], int E[]){
+    int i,j,k, contador=0, contadorarray=0;
+    
+    for(i=0; i< TAMANHO; ++i){
+        for(j=0; j< TAMANHO; ++j){
+            if(A[i]==B[j]){
+                contador+=1;
+            }
+        }
+        if(contador==1){
+            E[contadorarray]=A[i];
+            contadorarray+=1;
+        }
+        else{
+            //implentar resto do codigo aqui XD
+        }
+        contador=0;
+    }
+    
+}
+
+
 int main(int argc, char** argv) {
     int A[TAMANHO];
     int B[TAMANHO];
     int C[TAMANHO];
+    int D[TAMANHO];
+    int E[TAMANHO];
+    int i;
     
     arrayA(A);
-    puts("");
+    puts("------------------------------------------------------------------------");
     arrayB(B);
-    puts("");
     arrayC(A,B,C);
+    arrayD(A,B,D);
+    arrayE(A,B,E);
+    
+    for(i=0; i<TAMANHO; ++i){
+        printf("Array E: %d", E[i]);
+    }
     
     return (EXIT_SUCCESS);
 }
