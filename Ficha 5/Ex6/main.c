744d33abfdde37b4eac194b110f6fb1e6ec224bd
Miguel Costa <nerdxontheline@outlook.com>
2017-11-10 17:02:27 +0000

    Ex6

--- Ficha 5/Ex6/main.c
+++ Ficha 5/Ex6/main.c
@@ -13,12 +13,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "mytools.h"
+#define TAMANHO 10
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    double valores[TAMANHO];
+    int i;
+    char opcao;
+    
+    for(i=0; i< TAMANHO; ++i){
+        printf("Introduza um valor: ");
+        scanf("%lf", &valores[i]);
+    }
+    
+    printf("Deseja verificar os numeros pares ou impares ? (P/I)");
+    
+    
     return (EXIT_SUCCESS);
 }
 
