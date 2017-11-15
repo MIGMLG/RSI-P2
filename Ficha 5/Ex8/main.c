96964e89efc9a4f9fe64351bdf1facc8183cf023
Miguel Costa <nerdxontheline@outlook.com>
2017-11-15 19:21:43 +0000

    Ex8

--- Ficha 5/Ex8/main.c
+++ Ficha 5/Ex8/main.c
@@ -13,6 +13,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define MAX 1000
 /*
  * 
@@ -35,11 +36,65 @@ int max(){
     return valor;
 }
 
+void preencherArray(int array[], int max){
+    int i;
+    for(i=0; i<(max-1); ++i){
+        array[i]=i+2;
+    }
+}
+
+void resultado(int valor, int max, int array[]){
+    int i, k;
+    int j;
+    
+    for(i=0; i<(valor-1); ++i){
+        if(array[i]>max){
+            break;
+        }
+        else if(array[i]>0){
+            for(j=2; j<=(valor/array[i]); ++j){
+                for(k=0; k<=(valor-1); ++k){
+                    if((array[i]*j)==array[k]){
+                        array[k]=0;
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+            }
+        }
+        else{ 
+            continue;
+        }
+    }
+  
+}
+
+void print(int valor, int array[]){
+    int i;
+    printf("Primos: ");
+    for(i=0; i<(valor-1); ++i){
+        if(array[i]>0){
+            printf("%d, ", array[i]);
+        }
+    }
+    
+}
+
 int main(int argc, char** argv) {
-    int valor;
+    int valor, valormax, array[MAX];
+    
+    
+    int i;
     
     valor=max();
-    printf("Valor: %d", valor);
+    valormax=sqrt(valor);
+    preencherArray(array, valor);
+    resultado(valor, valormax, array);
+    print(valor, array);
+    
+    
     return (EXIT_SUCCESS);
 }
 
