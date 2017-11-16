b6218bd537fe59e589fff7b53590c275fcf7b5e5
Miguel Costa <nerdxontheline@outlook.com>
2017-11-16 22:18:15 +0000

    Ex9

---
+++ Ficha 6/Ex9/main.c
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Miguel Costa
+ *
+ * Created on 16 de Novembro de 2017, 22:15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+#define TAMANHO 25
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char frase1[TAMANHO];
+    char frase2[TAMANHO];
+ 
+    
+    printf("Escreva algo: ");
+    lerString(frase1, TAMANHO);
+    printf("Escreva algo: ");
+    lerString(frase2, TAMANHO);
+    
+    return (EXIT_SUCCESS);
+}
+
