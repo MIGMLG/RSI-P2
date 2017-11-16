696ec99272c1fb2dfa595967a06afe1bd73c221f
Miguel Costa <nerdxontheline@outlook.com>
2017-11-16 22:36:31 +0000

    Ex8

---
+++ Ficha 6/Ex8/utils.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   utils.h
+ * Author: aluno
+ *
+ * Created on November 16, 2017, 11:11 AM
+ */
+
+#ifndef UTILS_H
+#define	UTILS_H
+
+void clean_buffer();
+int lerString(char *string, int max);
+void countChar(char array[], char letra);
+
+#endif	/* UTILS_H */
+
